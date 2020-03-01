29f018bdf04f5ca0aaae44fe8b8527b108a2f69b
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-03-01 13:32:07 -0500

    first commit

---
+++ src/test/java/tests/MyFirstTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
